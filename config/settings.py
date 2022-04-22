2412bfb0fa1d35be07f0e23dd228aa11659777e3
jdrae <draejang@gmail.com>
2022-04-22 10:36:13 +0900

    add form css & design directory

--- config/settings.py
+++ config/settings.py
@@ -83,15 +83,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'main': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
+    },
+    "test": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+DATABASES['default'] = DATABASES[os.environ.get('DJANGO_DB')]
 
 
 # Password validation
